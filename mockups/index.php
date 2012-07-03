c34d4e1a028de9ff0739a94d451748289436065b
Nikolay Garanko <n.garanko@solin.nl>
2012-07-03 16:35:38 +0200

    Requisted changes to mockup

--- mockups/index.php
+++ mockups/index.php
@@ -13,6 +13,367 @@ $sub_columns = array("0001", "0002", "0003", "0004", "0005");
 
 $shown_subs = array("Toetsen");
 
+$show_hide = array("Toetsen");
+
+$users = array(
+
+        //////////////////////////////////////////////////////////////////
+    'First name Last Name' => array(
+            "Leerroute met eenheden" => array(
+                true, // 0001
+                true, // 0002
+                true, // 0003
+                true, // 004
+                true  // 005
+            ),
+            "Wiki's" => array(
+                true, // 0001
+                true, // 0002
+                true, // 0003
+                false, // 004
+                false  // 005
+            ),
+            "Films" => array(
+                true, // 0001
+                true, // 0002
+                true, // 0003
+                false, // 004
+                false  // 005
+            ),
+            "Toetsen" => array(
+                false, // 0001
+                false, // 0002
+                true, // 0003
+                false, // 004
+                false  // 005
+            )
+        ),
+        //////////////////////////////////////////////////////////////////
+        
+        //////////////////////////////////////////////////////////////////
+        'First name Last Name8' => array(
+                "Leerroute met eenheden" => array(
+                        true, // 0001
+                        true, // 0002
+                        true, // 0003
+                        true, // 004
+                        true  // 005
+                ),
+                "Wiki's" => array(
+                        true, // 0001
+                        true, // 0002
+                        true, // 0003
+                        false, // 004
+                        false  // 005
+                ),
+                "Films" => array(
+                        true, // 0001
+                        true, // 0002
+                        true, // 0003
+                        true, // 004
+                        true  // 005
+                ),
+                "Toetsen" => array(
+                        false, // 0001
+                        false, // 0002
+                        false, // 0003
+                        true, // 004
+                        false  // 005
+                )
+        ),
+        //////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////
+    'First name Last Name9' => array(
+            "Leerroute met eenheden" => array(
+                true, // 0001
+                true, // 0002
+                true, // 0003
+                false, // 004
+                true  // 005
+            ),
+            "Wiki's" => array(
+                true, // 0001
+                true, // 0002
+                true, // 0003
+                false, // 004
+                false  // 005
+            ),
+            "Films" => array(
+                true, // 0001
+                true, // 0002
+                true, // 0003
+                true, // 004
+                true  // 005
+            ),
+            "Toetsen" => array(
+                false, // 0001
+                false, // 0002
+                false, // 0003
+                false, // 004
+                false  // 005
+            )
+        ),
+        //////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////
+    'First name Last Name7' => array(
+            "Leerroute met eenheden" => array(
+                true, // 0001
+                true, // 0002
+                true, // 0003
+                true, // 004
+                true  // 005
+            ),
+            "Wiki's" => array(
+                true, // 0001
+                true, // 0002
+                true, // 0003
+                false, // 004
+                false  // 005
+            ),
+            "Films" => array(
+                true, // 0001
+                true, // 0002
+                true, // 0003
+                true, // 004
+                true  // 005
+            ),
+            "Toetsen" => array(
+                false, // 0001
+                false, // 0002
+                false, // 0003
+                false, // 004
+                false  // 005
+            )
+        ),
+        //////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////
+    'First name Last Name6' => array(
+            "Leerroute met eenheden" => array(
+                true, // 0001
+                true, // 0002
+                true, // 0003
+                true, // 004
+                true  // 005
+            ),
+            "Wiki's" => array(
+                true, // 0001
+                true, // 0002
+                true, // 0003
+                false, // 004
+                false  // 005
+            ),
+            "Films" => array(
+                true, // 0001
+                true, // 0002
+                true, // 0003
+                true, // 004
+                true  // 005
+            ),
+            "Toetsen" => array(
+                false, // 0001
+                false, // 0002
+                false, // 0003
+                false, // 004
+                false  // 005
+            )
+        ),
+        //////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////
+    'First name Last Name5' => array(
+            "Leerroute met eenheden" => array(
+                true, // 0001
+                true, // 0002
+                true, // 0003
+                true, // 004
+                true  // 005
+            ),
+            "Wiki's" => array(
+                true, // 0001
+                true, // 0002
+                true, // 0003
+                false, // 004
+                false  // 005
+            ),
+            "Films" => array(
+                true, // 0001
+                true, // 0002
+                true, // 0003
+                true, // 004
+                true  // 005
+            ),
+            "Toetsen" => array(
+                false, // 0001
+                false, // 0002
+                false, // 0003
+                false, // 004
+                false  // 005
+            )
+        ),
+        //////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////
+    'First name Last Name4' => array(
+            "Leerroute met eenheden" => array(
+                true, // 0001
+                true, // 0002
+                true, // 0003
+                true, // 004
+                true  // 005
+            ),
+            "Wiki's" => array(
+                true, // 0001
+                true, // 0002
+                true, // 0003
+                false, // 004
+                false  // 005
+            ),
+            "Films" => array(
+                true, // 0001
+                true, // 0002
+                true, // 0003
+                true, // 004
+                true  // 005
+            ),
+            "Toetsen" => array(
+                false, // 0001
+                false, // 0002
+                false, // 0003
+                false, // 004
+                false  // 005
+            )
+        ),
+        //////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////
+    'First name Last Name3' => array(
+            "Leerroute met eenheden" => array(
+                true, // 0001
+                true, // 0002
+                true, // 0003
+                true, // 004
+                true  // 005
+            ),
+            "Wiki's" => array(
+                true, // 0001
+                true, // 0002
+                true, // 0003
+                false, // 004
+                false  // 005
+            ),
+            "Films" => array(
+                true, // 0001
+                true, // 0002
+                true, // 0003
+                true, // 004
+                true  // 005
+            ),
+            "Toetsen" => array(
+                false, // 0001
+                false, // 0002
+                false, // 0003
+                false, // 004
+                false  // 005
+            )
+        ),
+        //////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////
+    'First name Last Name2' => array(
+            "Leerroute met eenheden" => array(
+                true, // 0001
+                true, // 0002
+                true, // 0003
+                true, // 004
+                true  // 005
+            ),
+            "Wiki's" => array(
+                true, // 0001
+                true, // 0002
+                true, // 0003
+                false, // 004
+                false  // 005
+            ),
+            "Films" => array(
+                true, // 0001
+                true, // 0002
+                true, // 0003
+                true, // 004
+                true  // 005
+            ),
+            "Toetsen" => array(
+                false, // 0001
+                false, // 0002
+                false, // 0003
+                false, // 004
+                false  // 005
+            )
+        ),
+        //////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////
+    'First name Last Name1' => array(
+            "Leerroute met eenheden" => array(
+                true, // 0001
+                true, // 0002
+                true, // 0003
+                true, // 004
+                true  // 005
+            ),
+            "Wiki's" => array(
+                true, // 0001
+                true, // 0002
+                true, // 0003
+                false, // 004
+                false  // 005
+            ),
+            "Films" => array(
+                true, // 0001
+                true, // 0002
+                true, // 0003
+                true, // 004
+                true  // 005
+            ),
+            "Toetsen" => array(
+                false, // 0001
+                false, // 0002
+                false, // 0003
+                false, // 004
+                false  // 005
+            )
+        ),
+        //////////////////////////////////////////////////////////////////
+);
+
+/***
+ * Function get_status_column
+ * Checks column  values and returns it's stauts
+ * 
+ * @param $column, array of true/false values
+ */
+function get_status_column($column) {
+    $status = 'ok icon-green';
+
+    // Check if there is at least one 'false' in $column
+    if (in_array(false, $column)) {
+        $status = 'adjust icon-yellow';
+
+        // Check if there is at least one 'true' in $column
+        if (!in_array(true, $column)) {
+            $status = 'remove icon-red';
+        }
+    }
+
+    return $status;
+}
+
+/**
+ * Function get_status
+ * Returns status string according to selection
+ * 
+ * @param $column, array of true/false values
+ * @param $sub_column, name of sub column
+ */
+function get_status($column, $sub_column, $sub_columns) {
+    $index = array_search($sub_column, $sub_columns);
+
+    return ($column[$index]) ? 'ok icon-green' : 'remove icon-red';
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -36,17 +397,21 @@ $shown_subs = array("Toetsen");
                       $class = $classes[$column];
                       ?>
                       <th><?php echo $column; ?><br />
-                      <?php if (!$show) {?>
-                      <a href="#" id="<?php echo $class; ?>" class="show-all" style="color: white">
-                      Toon
-                      <i class="icon-arrow-right icon-white"></i>
-                      <?php } else {?>
-                      <a href="#" style="color: white" id="<?php echo $class; ?>" class="show-all open">
-                          <i class="icon-arrow-left icon-white"></i>
-                          Verberg
-                      <?php } ?>
+                      <?php if (in_array($column, $show_hide)) {?>
+                          <?php if (!$show) {?>
+                          <a href="#" id="<?php echo $class; ?>" class="show-all" style="color: white">
+                          Toon
+                          <i class="icon-arrow-right icon-white"></i>
+                          <?php } else {?>
+                          <a href="#" style="color: white" id="<?php echo $class; ?>" class="show-all open">
+                              <i class="icon-arrow-left icon-white"></i>
+                              Verberg
+                          <?php } ?>
+                      <?php }?>
                       </a><br />
-                      <a href="#" class="global-toggler"><i class="icon icon-ok icon-white"></i></a>
+                      <button class="btn btn-primary">
+                          <i class="icon icon-ok icon-white"></i>
+                      </button>
                       </th>
                       <?php
 
@@ -54,7 +419,10 @@ $shown_subs = array("Toetsen");
                           ?>
                   <th class="sub <?php echo $class;?>" <?php if (!$show) {?>style="display: none"<?php }?>>&nbsp;<br />
                       <?php echo $sub_column; ?><br />
-                       <a href="#" class="global-toggler"><i class="icon icon-ok icon-white"></i></a>
+                      
+                      <button class="btn btn-primary">
+                          <i class="icon icon-ok icon-white"></i>
+                      </button>
                   </th>
                           <?php 
                       }
@@ -64,11 +432,15 @@ $shown_subs = array("Toetsen");
               </tr>
           </thead>
           <tbody>
-              <?php for ($i=0; $i<50; $i++) {?>
+              <?php foreach ($users as $name=>$data) {?>
               <!---------- row ----------------------------------------------->
               <tr>
-                  <td>Last Name First Name
-                      <a href="#" class="toggler" style="margin-left:20px;"><i class="icon icon-ok icon-green"></i></a>
+                  <td><?php echo $name; ?>
+                      
+                      <button class="btn btn-info">
+                          <i class="icon icon-ok icon-white"></i>
+                      </button>
+                      
                   </td>
               <?php
               foreach ($columns as $column) {
@@ -76,13 +448,14 @@ $shown_subs = array("Toetsen");
                   $class = $classes[$column];
               ?>
                   <td>
-                      <a href="#" class="toggler" data-original-title="<?php echo $column; ?>"><i class="icon icon-ok icon-green"></i></a>
+                      <a href="#" class="toggler" data-original-title="<?php echo $column; ?>"><i class="icon icon-<?php echo get_status_column($data[$column]); ?>"></i></a>
                   </td>
 
                   <?php foreach ($sub_columns as $sub_column) {?>
                   <!--  subcol -->
                   <td class="sub <?php echo $class;?>" <?php if (!$show) {?>style="display: none" <?php } ?>>
-                      <a href="#" class="toggler" data-original-title="<?php echo $sub_column; ?>"><i class="icon icon-ok icon-green"></i></a>
+                      <a href="#" class="toggler" data-original-title="<?php echo $sub_column; ?>">
+                      <i class="icon icon-<?php  echo get_status($data[$column], $sub_column, $sub_columns);?>"></i></a>
                   </td>
                   <!-- subcol eof -->
                   <?php } ?>
