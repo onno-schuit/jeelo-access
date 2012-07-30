421a51951d6b31e10e7c5a2cbfa9f51322e28791
Nick <nick@Ukraine.local>
2012-07-30 12:41:08 +0200

    Fixed  callbacks issue #11

--- package/mod/jeelo/db/install.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function xmldb_jeelo_install() {
-  global $DB;
- 
-  //$DB->get_records_sql(sprintf("INSERT INTO jeelo_access_defaults (key, value) VALUES('%s', '%s');", 'access', '0'));
-  //$DB->get_records_sql(sprintf("INSERT INTO jeelo_access_defaults (key, value) VALUES('%s', '%s');", 'expanded', 'quiz'));
-}
-
--- package/mod/jeelo/settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-  /*
-defined('MOODLE_INTERNAL') || die;
-
-require_once($CFG->dirroot . '/mod/assign/adminlib.php');
-
-$ADMIN->add('modules', new admin_category('jeeloplugins',
-                "TeST", !$module->visible));
-  */
