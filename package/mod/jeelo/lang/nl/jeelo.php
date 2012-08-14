9670531cfcd8c03dc30a56b8332535d9dc9deda8
Onno Schuit <o.schuit@solin.nl>
2012-08-14 10:01:42 +0200

    Changed a few language strings

--- package/mod/jeelo/lang/nl/jeelo.php
+++ package/mod/jeelo/lang/nl/jeelo.php
@@ -1,13 +1,13 @@
 <?php
   // example contents for lang/en/jeelo.php
-  $string['modulename'] = "Paneel Individuele Toegang";
-  $string['modulenameplural'] = 'Panelen Individuele Toegang';
+  $string['modulename'] = "Toegang leerlingen";
+  $string['modulenameplural'] = 'Toegang leerlingen';
   $string['modulename_help'] = 'Gebruik deze module om de toegang tot activiteiten voor specifieke leerlingen te (ont)sluiten';
   $string['newmodulefieldset'] = 'Custom example fieldset';
-  $string['newmodulename'] = 'Paneel Individuele Toegang';
+  $string['newmodulename'] = 'Toegang leerlingen';
   $string['newmodulename_help'] = 'This is the content of the help tooltip associated with the newmodulename field. Markdown syntax is supported.';
-  $string['newmodule'] = 'Paneel Individuele Toegang';
-  $string['pluginadministration'] = 'Beheer Paneel Individuele Toegang';
-  $string['pluginname'] = 'Paneel Individuele Toegang';
-  $string['access_required'] = "You do not have permission to open this page";
+  $string['newmodule'] = 'Toegang leerlingen';
+  $string['pluginadministration'] = 'Beheer Toegang leerlingen';
+  $string['pluginname'] = 'Toegang leerlingen';
+  $string['access_required'] = "Je hebt geen toegang tot deze pagina";
 ?>
