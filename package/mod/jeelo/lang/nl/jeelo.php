1e43c6665daee8c3f885262417dc06b8b75c12f0
Onno Schuit <o.schuit@solin.nl>
2012-08-27 16:10:23 +0200

    Added Dutch translation

--- package/mod/jeelo/lang/nl/jeelo.php
+++ package/mod/jeelo/lang/nl/jeelo.php
@@ -12,6 +12,6 @@
   $string['access_required'] = "Je hebt geen toegang tot deze pagina";
 
 
-$string['status_ok'] = "<strong>OK!</strong> Saved.";
-$string['status_err'] = "<strong>Warning!</strong> Data wasn't saved.";
+$string['status_ok'] = "<strong>OK!</strong> Bewaard";
+$string['status_err'] = "<strong>Waarschuwing!</strong> Wijzigingen niet bewaard.";
 ?>
