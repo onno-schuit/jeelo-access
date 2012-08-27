89674d513c90ac032e2a07ab6a4fdd1fae3c55e6
Onno Schuit <o.schuit@solin.nl>
2012-08-27 16:25:20 +0200

    Changed Dutch mod title

--- package/mod/jeelo/lang/nl/jeelo.php
+++ package/mod/jeelo/lang/nl/jeelo.php
@@ -1,17 +1,17 @@
 <?php
   // example contents for lang/en/jeelo.php
-  $string['modulename'] = "Toegang leerlingen";
-  $string['modulenameplural'] = 'Toegang leerlingen';
+  $string['modulename'] = "Leerlingenpaneel";
+  $string['modulenameplural'] = 'Leerlingenpanelen';
   $string['modulename_help'] = 'Gebruik deze module om de toegang tot activiteiten voor specifieke leerlingen te (ont)sluiten';
   $string['newmodulefieldset'] = 'Custom example fieldset';
-  $string['newmodulename'] = 'Toegang leerlingen';
+  $string['newmodulename'] = 'Leerlingenpaneel';
   $string['newmodulename_help'] = 'This is the content of the help tooltip associated with the newmodulename field. Markdown syntax is supported.';
-  $string['newmodule'] = 'Toegang leerlingen';
-  $string['pluginadministration'] = 'Beheer Toegang leerlingen';
-  $string['pluginname'] = 'Toegang leerlingen';
+  $string['newmodule'] = 'Leerlingenpaneel';
+  $string['pluginadministration'] = 'Beheer Leerlingenpanelen';
+  $string['pluginname'] = 'Leerlingenpaneel';
   $string['access_required'] = "Je hebt geen toegang tot deze pagina";
 
 
-$string['status_ok'] = "<strong>OK!</strong> Bewaard";
-$string['status_err'] = "<strong>Waarschuwing!</strong> Wijzigingen niet bewaard.";
+  $string['status_ok'] = "<strong>OK!</strong> Bewaard";
+  $string['status_err'] = "<strong>Waarschuwing!</strong> Wijzigingen niet bewaard.";
 ?>
