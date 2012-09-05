b4727e2ce3c98c9404344119fa8d6cf9a4e89dbe
Onno Schuit <o.schuit@solin.nl>
2012-09-05 12:07:31 +0200

    Updated language strings

--- package/mod/jeelo/lang/nl/jeelo.php
+++ package/mod/jeelo/lang/nl/jeelo.php
@@ -1,13 +1,13 @@
 <?php
   // example contents for lang/en/jeelo.php
   $string['modulename'] = "Leerlingenpaneel";
-  $string['modulenameplural'] = 'Leerlingenpanelen';
+  $string['modulenameplural'] = 'Leerlingenpaneel';
   $string['modulename_help'] = 'Gebruik deze module om de toegang tot activiteiten voor specifieke leerlingen te (ont)sluiten';
   $string['newmodulefieldset'] = 'Custom example fieldset';
   $string['newmodulename'] = 'Leerlingenpaneel';
   $string['newmodulename_help'] = 'This is the content of the help tooltip associated with the newmodulename field. Markdown syntax is supported.';
   $string['newmodule'] = 'Leerlingenpaneel';
-  $string['pluginadministration'] = 'Beheer Leerlingenpanelen';
+  $string['pluginadministration'] = 'Beheer Leerlingenpaneel';
   $string['pluginname'] = 'Leerlingenpaneel';
   $string['access_required'] = "Je hebt geen toegang tot deze pagina";
 
