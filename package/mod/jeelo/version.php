ced7d74703e90041ef9da431cc402d789214c1fc
Onno Schuit <o.schuit@solin.nl>
2012-09-05 11:21:23 +0200

    Bumped up version number to force Moodle to read out language strings again

--- package/mod/jeelo/version.php
+++ package/mod/jeelo/version.php
@@ -2,7 +2,7 @@
   // example contents for version.php
   defined('MOODLE_INTERNAL') || die();
 
-  $module->version   = 2012070400;       // The current module version (Date: YYYYMMDDXX)
+  $module->version   = 2012090500;       // The current module version (Date: YYYYMMDDXX)
   $module->requires  = 2011112900;       // Requires this Moodle version
   $module->component = 'mod_jeelo';       // Full name of the plugin (used for diagnostics)
   $module->cron      = 0;
